88a83c86df3bf7149ed84b31d94356f6ad028a02
fishonamos <amosfishon@gmail.com>
2024-02-19 12:51:50 +0100

    Modified the code to send a request to /api/:date?

--- timestamp_microservice/index.js
+++ timestamp_microservice/index.js
@@ -19,8 +19,9 @@ app.get("/", function (req, res) {
 });
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date?", function (req, res) {
+  let date = req.params.date;
+  res.json({date: date});
 });
 
 // Listen on port set in environment variable or default to 3000
